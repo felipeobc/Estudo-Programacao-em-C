7e624321ae64c76ff5dd2c1d02c6310f08b59b8c
Felipe Oliveira Bueno de Camargo <felipe.c@outlook.com>
2017-08-18 21:53:19 -0300

    Update Ex Vetor 02.c

--- Aula Algoristimos II/Ex Vetor 02.c
+++ Aula Algoristimos II/Ex Vetor 02.c
@@ -4,28 +4,30 @@
 
 int main () {
 
-     int Indice, Numeros_Int[N], Numeros_Medidas = 0;
+     int Indice, Vetor[N], Numeros_Medidas = 0, Vetor_Aux[N], Aux ;
 
     printf("Digite quantos numeros voce quer armazenar: ");
     scanf("%d", &Numeros_Medidas);
 
     for(Indice = 0; Indice < Numeros_Medidas; Indice++){
         printf("Digite o %d Numero: ", Indice +1);
-        scanf("%d", &Numeros_Int[Indice]);
+        scanf("%d", &Vetor[Indice]);
 
     }
-        printf("Vetor Digitado V[%d] = ", Numeros_Medidas);
 
-    for (Indice = 0; Indice < Numeros_Medidas; Indice++){
+    for(Indice = Numeros_Medidas - 1; Indice >= 0; Indice--){
 
-            printf("%d ", Numeros_Int[Indice]);
+        Aux = Vetor[Indice];
+        Vetor[Indice] = Vetor_Aux[Indice - 1];
+        Vetor_Aux[Indice] = Aux;
+
     }
+    printf("Vetor Trocado: ");
+    for(Indice = 0 ; Indice < 0; Indice++){
 
-        printf("\nVetor Alaterado V[%d] =  ", Numeros_Medidas);
+        printf("%d ", Vetor_Aux[Indice]);
 
-    for (Indice = Numeros_Medidas -1 ; Indice >= 0; Indice--){
 
-            printf("%d ", Numeros_Int[Indice]);
     }
     printf("\n");
     system("pause");
